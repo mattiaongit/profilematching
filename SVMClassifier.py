61482d4691dac2ecc0707e311c7c25bdef40b0a8
tia <mattiadmr@gmail.com>
2014-11-05 16:37:59 +0100

    self as argument

--- SVMClassifier.py
+++ SVMClassifier.py
@@ -12,20 +12,20 @@ class SVMClassifier:
     self.clf = []
 
 
-  def splitDataTrainingTest(fraction):
+  def splitDataTrainingTest(self,fraction):
     #Split training set from data
     self.X_train, self.X_test, self.y_train, self.y_test = cross_validation.train_test_split( self.data, self.targets, test_size = len(self.data)/fraction , random_state = 0 )
 
 
-  def normalizeData(axis,type):
+  def normalizeData(self,axis,type):
     # self.X_train = preprocessing.scale(X_train)
     # self.X_test = preprocessing.scale(X_test)
     pass
 
-  def train(params):
+  def train(self,params):
     self.clf = svm.SVC(**params)
     self.clf.fit(self.X_train,self.X_test)
 
 
-  def score(scoring, tuned_parameters):
+  def score(self,scoring, tuned_parameters):
     pass
