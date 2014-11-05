11c2fed39cae9b90c003fdcc6179a8737a0d7ccb
tia <mattiadmr@gmail.com>
2014-11-05 15:39:51 +0100

    initial commit

---
+++ matcher.py
@@ -0,0 +1,29 @@
+from features import *
+from distances import *
+
+from random import shuffle
+from itertools import combinations
+
+import pymongo
+
+
+#IMPORT DATA FROM DB
+connection = pymongo.Connection()
+db = connection['alternion']
+dbprofiles = db.profiles.find({},{'_id':0})
+profilesPairs = dict()
+socialNetowrks = set()
+
+# UTILS
+def shuffleProfiles(profilePair):
+  l1,l2 = zip(*profilePair)
+  l = list(l2)
+  shuffle(l)
+  l2 = tuple(l)
+  return  list(zip(l1,l2))
+
+
+features_functions = [ull, uucl, alphabetDistribution, eachFingerRate, rowsRate,lambda x : sameRate(x,granularitiesFunction=sameHand),lambda x : sameRate(x,granularitiesFunction=sameFinger),levenshtein,jaccard,shannonEntropy,lcsubstring,lcs]
+
+def vectorize(pair):
+    return [f(pair) for f in features_fucntions]
