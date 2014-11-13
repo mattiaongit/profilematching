b8baeecf761f647c3ae7b1031ebd2915f54a182e
tia <mattiadmr@gmail.com>
2014-11-13 15:03:58 +0100

    grid searching

--- matcher.py
+++ matcher.py
@@ -72,7 +72,7 @@ SVMClf1 = SVMClassifier(data, targets)
 SVMClf1.splitDataTrainingTest(2)
 
 # scaler from sklearn, es: MinMaxScaler, StandardScaler
-SVMClf1.normalizeData(sklearn.preprocessing.MinMaxScaler)
+SVMClf1.normalizeData(sklearn.preprocessing.StandardScaler)
 
 
 tuning_parameters = [{'kernel': ['rbf'], 'gamma': [1e-3, 1e-4],
@@ -80,10 +80,11 @@ tuning_parameters = [{'kernel': ['rbf'], 'gamma': [1e-3, 1e-4],
                     {'kernel': ['linear'], 'C': [1, 10, 100, 1000]}]
 scores = ['f1', 'recall']
 
-#SVMClf1.gridSearch(tuning_parameters, scores)
+best_estimator = SVMClf1.gridSearch(tuning_parameters, scores)
 
 
 params = {'C':10, 'cache_size':200, 'class_weight':None, 'coef0':0.0, 'degree':3, 'gamma':0.0001, 'kernel':'linear', 'max_iter':-1, 'probability':False, 'random_state':None, 'shrinking':True, 'tol':0.001, 'verbose':False}
-SVMClf1.train(params)
+
+SVMClf1.train(*best_estimator)
 
 SVMClf1.test(output = True)
