da93b6868a532e411b6f1a85c333ebd29a4f5e5e
tia <mattiadmr@gmail.com>
2014-11-15 14:26:29 +0100

    stats debugged

--- stats.py
+++ stats.py
@@ -15,8 +15,6 @@ else:
 def distribution(v):
   v = list(v) # if v is a tuple
   if len(v) < 2:
-    print "distribution fail"
-    print v
     v.append(v[0])
     #return [v[0]]
   return [stf(v) for stf in stats_functions]
