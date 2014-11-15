1c2b7988df172f7ab262aaa2757d3398c8204c91
tia <mattiadmr@gmail.com>
2014-11-15 13:52:13 +0100

    re enable all features

--- PreProcessor.py
+++ PreProcessor.py
@@ -59,7 +59,7 @@ class PreProcessor():
 
 
 	def vectorizeData(self,timer = False):
-		self.selected_features = self.features['humanlimitations'] #+ self.features['exogenous']['qwerty'] + self.features['endogenous']
+		self.selected_features = self.features['humanlimitations'] + self.features['exogenous']['qwerty'] + self.features['endogenous']
 
 		counter = 0
 		#self.data = map(self.vectorize, sample)
