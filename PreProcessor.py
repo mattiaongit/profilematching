a80472364d0405a1da538326d5153703bfde99d9
tia <mattiadmr@gmail.com>
2014-11-15 03:55:47 +0100

    trying popular similar class

--- PreProcessor.py
+++ PreProcessor.py
@@ -37,7 +37,7 @@ class PreProcessor():
 		print('Preprocessing data')
 		if self.priors == 0:
 			profiles =[(profile['Google+']['username'],[v['username'] for k,v in profile.items() if k == 'Youtube' and len(v['username']) > 0]) for profile in list(self.rawdata) if "Google+" in profile.keys()]
-			profiles = [x for x in profiles if len(x[1]) > 0 and len(x[0]) > 0 ] # filter(len, profiles)
+			profiles = [x for x in profiles if len(x[1]) > 0 and len(x) > 0 ] # filter(len, profiles)
 			print(profiles)
 			candidates, priors = zip(*profiles)
 			tmp = list(candidates)
