7860b51664d808cf0a18007a90d0e17ab4b759af
tia <mattiadmr@gmail.com>
2014-11-15 22:26:38 +0100

    all combinations ... praying god

--- PreProcessor.py
+++ PreProcessor.py
@@ -48,6 +48,8 @@ class PreProcessor():
 				for cKey,cUsername in profile.items():
 					profiles.append((cUsername, [pUsername for pKey,pUsername in profile.items() if cKey != pKey]))
 
+
+			print(profiles[:2])
 			candidates, priors = zip(*profiles)
 			tmp = list(candidates)
 			shuffle(tmp)
