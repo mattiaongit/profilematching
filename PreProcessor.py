d40bfbece69c0df37355c682f10ec8905433c5df
tia <mattiadmr@gmail.com>
2014-11-16 16:38:35 +0100

    Added Features params

--- PreProcessor.py
+++ PreProcessor.py
@@ -24,11 +24,11 @@ class PreProcessor():
         self.filterFeatures = filterFeatures
 
 		self.features = {
-			'humanlimitations': [sameUsername, ull, uucl],
-			'exogenousqwerty': [lambda x,y: sameRate(x,y,granularitiesFunction=sameHand),lambda x,y : sameRate(x,y,granularitiesFunction=sameFinger),eachFingerRate,rowsRate],
-            'exogenousdvorak': [lambda x,y: sameRate(x,y,granularitiesFunction=sameHand,layout='dvorak'),lambda x,y : sameRate(x,y,granularitiesFunction=sameFinger,layout='dvorak'),lambda x,y: eachFingerRate(x,y,layout='dvorak'), lambda x,y: rowsRate(x,y,layout='dvorak')],
-			'endogenous': [alphabetDistribution,shannonEntropy,lcsubstring,lcs],
-			'distances' : [levenshtein, jaccard]
+        'humanlimitations': [sameUsername, ull, uucl],
+		'exogenousqwerty': [lambda x,y: sameRate(x,y,granularitiesFunction=sameHand),lambda x,y : sameRate(x,y,granularitiesFunction=sameFinger),eachFingerRate,rowsRate],
+        'exogenousdvorak': [lambda x,y: sameRate(x,y,granularitiesFunction=sameHand,layout='dvorak'),lambda x,y : sameRate(x,y,granularitiesFunction=sameFinger,layout='dvorak'),lambda x,y: eachFingerRate(x,y,layout='dvorak'), lambda x,y: rowsRate(x,y,layout='dvorak')],
+		'endogenous': [alphabetDistribution,shannonEntropy,lcsubstring,lcs],
+		'distances' : [levenshtein, jaccard]
 		}
 
 		self.selected_features = []
