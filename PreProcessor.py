238ff91146aa99a82d3db8807ec083623fc24244
tia <mattiadmr@gmail.com>
2014-11-16 16:40:09 +0100

    Added Features params

--- PreProcessor.py
+++ PreProcessor.py
@@ -23,13 +23,13 @@ class PreProcessor():
 
         self.filterFeatures = filterFeatures
 
-		self.features = {
+        self.features = {
         'humanlimitations': [sameUsername, ull, uucl],
-		'exogenousqwerty': [lambda x,y: sameRate(x,y,granularitiesFunction=sameHand),lambda x,y : sameRate(x,y,granularitiesFunction=sameFinger),eachFingerRate,rowsRate],
+        'exogenousqwerty': [lambda x,y: sameRate(x,y,granularitiesFunction=sameHand),lambda x,y : sameRate(x,y,granularitiesFunction=sameFinger),eachFingerRate,rowsRate],
         'exogenousdvorak': [lambda x,y: sameRate(x,y,granularitiesFunction=sameHand,layout='dvorak'),lambda x,y : sameRate(x,y,granularitiesFunction=sameFinger,layout='dvorak'),lambda x,y: eachFingerRate(x,y,layout='dvorak'), lambda x,y: rowsRate(x,y,layout='dvorak')],
-		'endogenous': [alphabetDistribution,shannonEntropy,lcsubstring,lcs],
-		'distances' : [levenshtein, jaccard]
-		}
+        'endogenous': [alphabetDistribution,shannonEntropy,lcsubstring,lcs],
+        'distances' : [levenshtein, jaccard]
+        }
 
 		self.selected_features = []
 
