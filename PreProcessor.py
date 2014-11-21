e13cc95a0969e67ad564f1c3a45b4dcad6838390
tia <mattiadmr@gmail.com>
2014-11-21 01:58:11 +0100

    PCA 10 comp

--- PreProcessor.py
+++ PreProcessor.py
@@ -90,54 +90,55 @@ class PreProcessor():
                 print("{0}0% done ... ({1}/{2}) samples".format(counter/(len(self.ppdata)/10),counter,len(self.ppdata)))
                 #print(self.vectorize(sample,debug = True))
 
-    def pca(self, ncomponents):
+    def pca(self, ncomponents, plot = False):
         print "pca!"
         pca = sklearn.decomposition.PCA(n_components=ncomponents)
         pca.fit(self.data)
         self.data = pca.transform(self.data)
 
-        np.random.seed(5)
+        if plot and n_components == 3:
+            np.random.seed(5)
 
-        centers = [[1, 1], [-1, -1], [1, -1]]
-        X = self.data
-        y = self.targets
+            centers = [[1, 1], [-1, -1], [1, -1]]
+            X = self.data
+            y = self.targets
 
-        fig = plt.figure(1, figsize=(8, 6))
-        plt.clf()
-        ax = Axes3D(fig, rect=[0, 0, .95, 1], elev=18, azim=54)
+            fig = plt.figure(1, figsize=(8, 6))
+            plt.clf()
+            ax = Axes3D(fig, rect=[0, 0, .95, 1], elev=18, azim=54)
 
-        plt.cla()
+            plt.cla()
 
-        for name, label in [('Sameuser', 0), ('Different', 1)]:
-            ax.text3D(X[y == label, 0].mean(),
-                      X[y == label, 1].mean() + 1.5,
-                      X[y == label, 2].mean(), name,
-                      horizontalalignment='center',
-                      bbox=dict(alpha=.5, edgecolor='w', facecolor='w'))
-        # Reorder the labels to have colors matching the cluster results
-        y = np.choose(y, [1, 2, 0]).astype(np.float)
-        ax.scatter(X[:, 0], X[:, 1], X[:, 2], c=y, cmap=plt.cm.spectral)
+            for name, label in [('Sameuser', 0), ('Different', 1)]:
+                ax.text3D(X[y == label, 0].mean(),
+                          X[y == label, 1].mean() + 1.5,
+                          X[y == label, 2].mean(), name,
+                          horizontalalignment='center',
+                          bbox=dict(alpha=.5, edgecolor='w', facecolor='w'))
+            # Reorder the labels to have colors matching the cluster results
+            y = np.choose(y, [1, 2, 0]).astype(np.float)
+            ax.scatter(X[:, 0], X[:, 1], X[:, 2], c=y, cmap=plt.cm.spectral)
 
-        x_surf = [X[:, 0].min(), X[:, 0].max(),
-                  X[:, 0].min(), X[:, 0].max()]
-        y_surf = [X[:, 0].max(), X[:, 0].max(),
-                  X[:, 0].min(), X[:, 0].min()]
-        x_surf = np.array(x_surf)
-        y_surf = np.array(y_surf)
-        v0 = pca.transform(pca.components_[0])
-        v0 /= v0[-1]
-        v1 = pca.transform(pca.components_[1])
-        v1 /= v1[-1]
+            x_surf = [X[:, 0].min(), X[:, 0].max(),
+                      X[:, 0].min(), X[:, 0].max()]
+            y_surf = [X[:, 0].max(), X[:, 0].max(),
+                      X[:, 0].min(), X[:, 0].min()]
+            x_surf = np.array(x_surf)
+            y_surf = np.array(y_surf)
+            v0 = pca.transform(pca.components_[0])
+            v0 /= v0[-1]
+            v1 = pca.transform(pca.components_[1])
+            v1 /= v1[-1]
 
-        ax.w_xaxis.set_ticklabels([])
-        ax.w_yaxis.set_ticklabels([])
-        ax.w_zaxis.set_ticklabels([])
+            ax.w_xaxis.set_ticklabels([])
+            ax.w_yaxis.set_ticklabels([])
+            ax.w_zaxis.set_ticklabels([])
 
-        plt.savefig("plot2D.png")
+            plt.savefig("plot2D.png")
 
 
     def datatargets(self):
         self.preprocess()
         self.vectorizeData(debug=True)
-        #self.pca(3)
+        self.pca(10)
         return self.data, self.targets
