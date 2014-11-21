6f56ad4a6e16137e174c72b2cf268ceb1c96339f
tia <mattiadmr@gmail.com>
2014-11-21 21:49:43 +0100

    only dist min 10

--- matcher.py
+++ matcher.py
@@ -9,7 +9,7 @@ filterCandidate = ['Google+']
 filterPriors = ['YouTube']
 filterFeatures = ['distances']#,'humanlimitations','exogenousqwerty','exogenousdvorak']
 
-pp = PreProcessor(minPriors=11, filterFeatures = filterFeatures)
+pp = PreProcessor(minPriors=10, filterFeatures = filterFeatures)
 data, targets = pp.datatargets()
 
 clf = Classifier('SGDClassifier',data, targets)
