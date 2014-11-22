20671f3ab51428d76616b2c17b1447f124fcda44
tia <mattiadmr@gmail.com>
2014-11-22 11:30:38 +0100

    PA only Dist features

--- matcher.py
+++ matcher.py
@@ -7,7 +7,7 @@ import pdb
 
 filterCandidate = ['Google+']
 filterPriors = ['YouTube']
-filterFeatures = ['distances','humanlimitations']#,'exogenousqwerty','exogenousdvorak']
+filterFeatures = ['distances']#,'humanlimitations']#,'exogenousqwerty','exogenousdvorak']
 
 pp = PreProcessor(minPriors=1, filterFeatures = filterFeatures)
 data, targets = pp.datatargets()
